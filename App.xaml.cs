b89691ede4772b5218d19a1252ac8c62264fbadb
thaverk <thaverk01@gmail.com>
2024-01-31 07:29:19 +0200

    Updated Assignment

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,7 +8,9 @@
 
             
             MainPage = new AppShell();
-           
+            
+            
+        
         }
     }
 }
--- Profile.xaml.cs
+++ Profile.xaml.cs
@@ -1,9 +1,25 @@
+
+
 namespace MyFirstMauiApplication;
 
 public partial class Profile : ContentPage
 {
-	public Profile()
+   
+    
+    public Profile()
 	{
 		InitializeComponent();
 	}
+
+    
+    
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    { 
+        
+        Routing.RegisterRoute(nameof(AppShell), typeof(AppShell));
+        //Routing.RegisterRoute("AppShell", typeof(AppShell));
+        
+        await Shell.Current.GoToAsync(nameof(AppShell));
+    }
 }
