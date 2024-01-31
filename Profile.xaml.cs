dce424a836ec6548f4cceaafae55d249537ae695
thaverk <thaverk01@gmail.com>
2024-01-31 15:47:21 +0200

    From Campus

--- Profile.xaml.cs
+++ Profile.xaml.cs
@@ -9,17 +9,18 @@ public partial class Profile : ContentPage
     public Profile()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute("HomePage", typeof(HomePage));
+    }
 
-    
-    
+
+
 
     private async void ImageButton_Clicked(object sender, EventArgs e)
-    { 
-        
-        Routing.RegisterRoute(nameof(AppShell), typeof(AppShell));
+    {
+
+        await Shell.Current.GoToAsync("HomePage");
         //Routing.RegisterRoute("AppShell", typeof(AppShell));
-        
-        await Shell.Current.GoToAsync(nameof(AppShell));
+        //await Shell.Current.Navigation.PushAsync( new AppShell());
+        //await Shell.Current.GoToAsync("Home");
     }
 }
